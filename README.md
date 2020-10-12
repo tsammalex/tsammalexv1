5990079dc238d4c89978944489241d1543164ed5
xrotwang <xrotwang@googlemail.com>
2020-10-12 16:09:30 +0200

    fleshed out names and images

--- README.md
+++ README.md
@@ -4,15 +4,21 @@
 
 
 [![Build Status](https://travis-ci.org/tsammalex/tsammalexv1.svg?branch=master)](https://travis-ci.org/tsammalex/tsammalexv1)
-![Glottolog: 0%](https://img.shields.io/badge/Glottolog-0%25-red.svg "Glottolog: 0%")
+![Glottolog: 99%](https://img.shields.io/badge/Glottolog-99%25-brightgreen.svg "Glottolog: 99%")
 ![Concepticon: 0%](https://img.shields.io/badge/Concepticon-0%25-red.svg "Concepticon: 0%")
-![Source: 0%](https://img.shields.io/badge/Source-0%25-red.svg "Source: 0%")
+![Source: 70%](https://img.shields.io/badge/Source-70%25-yellow.svg "Source: 70%")
 
-- **Varieties:** 0
-- **Concepts:** 0
-- **Lexemes:** 0
-- **Sources:** 0
-- **Synonymy:** 0.00
+- **Varieties:** 139
+- **Concepts:** 897
+- **Lexemes:** 5,502
+- **Sources:** 1,386
+- **Synonymy:** 1.98
+
+## Possible Improvements:
+
+
+
+- Entries missing sources: 1632/5502 (29.66%)
 
 # Contributors
 
--- lexibank_tsammalexv1.py
+++ lexibank_tsammalexv1.py
@@ -1,3 +1,5 @@
+import re
+
 from pathlib import Path
 import pylexibank
 import attr
@@ -5,22 +7,36 @@ import attr
 from csvw.dsv import UnicodeWriter
 from pytsammalex.gbif import GBIF
 from pytsammalex import lexibank
+import pylexibank
 
-# Customize your basic data.
-# if you need to store other data in columns than the lexibank defaults, then over-ride
-# the table type (pylexibank.[Language|Lexeme|Concept|Cognate|]) and add the required columns e.g.
-#
-#import attr
-#
+
 @attr.s
 class Taxon(lexibank.Taxon):
     Description = attr.ib(default=None)
+
+
+@attr.s
+class Name(pylexibank.Lexeme):
+    ipa = attr.ib(default=None)
+    grammatical_info = attr.ib(default=None)
+    plural_form = attr.ib(default=None)
+    basic_term = attr.ib(default=None)
+    meaning = attr.ib(default=None)
+    literal_translation = attr.ib(default=None)
+    usage = attr.ib(default=None)
+    linguistic_notes = attr.ib(default=None)
+    related_lexemes = attr.ib(default=None)
+    uses = attr.ib(default=None)
+    ethnobiological_notes = attr.ib(default=None)
+    comment = attr.ib(default=None)
+    source = attr.ib(default=None)
 
 
 class Dataset(lexibank.Dataset):
     dir = Path(__file__).parent
     id = "tsammalexv1"
     concept_class = Taxon
+    lexeme_class = Name
 
     def cmd_download(self, args):
         """
@@ -44,26 +60,42 @@ class Dataset(lexibank.Dataset):
         }
         for concept in self.concepts:
             args.writer.add_concept(Description=descs[concept['ID']], **concept)
-        return
-        for language in self.languages:
+
+        # TODO: add media!
+
+        for language in self.raw_dir.read_csv('languages.csv', dicts=True):
             args.writer.add_language(
-                ID=language['ID'],
-                Glottolog=language['Glottolog']
+                ID=language['id'],
+                Name=language['name'],
+                Glottocode=language['glottocode'],
+                Latitude=language['latitude'],
+                Longitude=language['longitude'],
             )
 
-        # add data
-        for row in pylexibank.progressbar(data):
-            # .. if you have segmentable data, replace `add_form` with `add_form_with_segments`
-            # .. TODO @Mattis, when should we use add_forms_from_value() instead?
-            lex = args.writer.add_form(
-                Language_ID=row['Language_ID'],
-                Parameter_ID=row['Parameter_ID'],
-                Value=row['Word'],
-                Form=row['Word'],
-                Source=[row['Source']],
+        args.writer.add_sources()
+
+        args.writer.cldf['FormTable', 'uses'].separator = ';'
+        uses = {r['id']: r['name'] for r in self.raw_dir.read_csv('uses.csv', dicts=True)}
+        for row in self.raw_dir.read_csv('names.csv', dicts=True):
+            refs = [s.replace('[]', '') for s in re.split('(?<=])[,;]\s*', row['refs__ids'])]
+            args.writer.add_form(
+                Language_ID=row['languages__id'],
+                Parameter_ID=row['taxa__id'],
+                Value=row['name'],
+                Form=row['name'],
+                Source=refs,
+                uses=[uses[u] for u in re.split('[,;]\s', row['uses__ids']) if u],
+                **{k: row[k] for k in attr.fields_dict(Name) if k in row and row[k]}
             )
-            # add cognates -- make sure Cognateset_ID is global!
-            args.writer.add_cognate(
-                lexeme=lex,
-                Cognateset_ID=row['Cognateset_ID']
-            )
+        self.add_image_schema(args.writer)
+        cdstar = self.raw_dir.read_json('cdstar.json')
+        for row in self.raw_dir.read_csv('images.csv', dicts=True):
+            args.writer.objects['images.csv'].append(dict(
+                ID=row['id'],
+                Taxon_ID=row['taxa__id'],
+                objid=cdstar[row['id']]['objid'],
+                bitsreamid=cdstar[row['id']]['original'],
+                tags=row['tags'].split(';'),
+                **{k: v for k, v in row.items()
+                   if k in ['mime_type', 'creator', 'date', 'place', 'permission', 'source']}
+            ))
